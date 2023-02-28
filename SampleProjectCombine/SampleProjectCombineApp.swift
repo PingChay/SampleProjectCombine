c74857dec603a24746a70a82b7af027d2bea1a14
Saran Nonkamjan <saran.non@gmail.com>
2023-02-28 14:48:13 +0700

    Initial Commit

---
+++ SampleProjectCombine/SampleProjectCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleProjectCombineApp.swift
+//  SampleProjectCombine
+//
+//  Created by Saran Nonkamjan on 28/2/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct SampleProjectCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
